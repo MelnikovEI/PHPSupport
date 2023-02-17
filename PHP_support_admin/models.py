08ea1034cd87d00cbca88c0e6c6e6c32ba76b462
MelnikovEI <MelnikovEI@ya.ru>
2023-02-17 20:49:04 +0300

    Add additional fields to order to send messages

--- PHP_support_admin/models.py
+++ PHP_support_admin/models.py
@@ -34,13 +34,16 @@ class Question(models.Model):
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='client', related_name='orders')
     contractor = models.ForeignKey(Contractor, on_delete=models.CASCADE, verbose_name='contractor', related_name='orders', null=True, blank=True)
-    question = models.ManyToManyField(Question, verbose_name='question', related_name='order', null=True, blank=True)
+    question = models.ManyToManyField(Question, verbose_name='question', related_name='order', blank=True)
     request = models.TextField('description of the request from client')
     access_info = models.CharField('web site access information', max_length=400)
     date_estimation = models.DateField(verbose_name='estimation date of completing job from contractor', null=True, blank=True)
     is_finished_by_contractor = models.BooleanField("is order finished from contractor's point of view", default=False)
     is_finished_by_client = models.BooleanField("is order finished from client's point of view", default=False)
     date_closed = models.DateField(verbose_name='date of closing the order by client', null=True, blank=True)
+
+    client_chat_id = models.IntegerField(verbose_name='chat id to send messages to client', null=True, blank=True)
+    contractor_chat_id = models.IntegerField(verbose_name='chat id to send messages to contractor', null=True, blank=True)
 
     def __str__(self):
         return f'{self.client.tg_account}_{self.id}'
